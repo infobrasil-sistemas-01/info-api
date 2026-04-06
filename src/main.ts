692240779f95e41e8b3fa66909a337262f86c24f
gabriel.bezerra <gabriel.camurca@outlook.com>
2026-04-06 17:44:19 -0300

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { apiReference } from '@scalar/nestjs-api-reference';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.setGlobalPrefix('api/v1');
 
   const config = new DocumentBuilder()
