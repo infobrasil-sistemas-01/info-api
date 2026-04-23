bb63958184bd1c584d794b93ec2a52d002476114
gabriel.bezerra <gabriel.camurca@outlook.com>
2026-04-23 08:38:23 -0300

    chore: clean inutil packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { SentryModule } from '@sentry/nestjs/setup';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProductModule } from './modules/product/product.module';
 import { OrderModule } from './modules/order/order.module';
@@ -10,7 +9,6 @@ import { AccountReceivableModule } from './modules/account-receivable/account-re
 
 @Module({
   imports: [
-    SentryModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
