ba159ccbee2dac13fb5cf9cb3b9a6f78c2996365
Gabriel Camurça Bezerra <gabriel.bezerra@infobrasil.local>
2026-08-31 14:14:23 -0300

    fix(fiscal-entry): import TenantConnectionModule and update swagger spec

--- src/modules/fiscal-entry/fiscal-entry.module.ts
+++ src/modules/fiscal-entry/fiscal-entry.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FiscalEntryController } from './fiscal-entry.controller';
 import { FiscalEntryService } from './fiscal-entry.service';
+import { TenantConnectionModule } from 'src/infra/database/tenant-connection.module';
 
 @Module({
+  imports: [TenantConnectionModule],
   controllers: [FiscalEntryController],
   providers: [FiscalEntryService],
   exports: [FiscalEntryService],
